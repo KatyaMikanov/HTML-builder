61bbb9b77b410113df8757874cb79f73217be82c
Katya Mikanov <kategubko@gmail.com>
2023-05-06 20:35:19 +0300

    feat: add solution to 04-copy-directiory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 
+const urlDestination = path.join(__dirname, 'files-copy');
+const urlSource = path.join(__dirname, 'files');
+
+const fsPromises = fs.promises;
+fsPromises.mkdir(urlDestination, { recursive: true }).then(function () {
+    console.log('Каталог успешно создан');
+}).catch(function () {
+    console.log('Не удалось создать каталог');
+});
+
+fs.readdir(urlSource, (err, files) => {
+
+    files.forEach((file) => {
+        if (err) {
+            console.error(err);
+        }
+        
+        const originalFilePath = path.join(__dirname, 'files', file)
+
+        fs.stat(originalFilePath, (err, el) => {
+            if (err) {
+                console.log(err);
+            }
+            if (el.isFile()) {
+                const writePath = path.join(urlDestination, file);
+                fs.promises.copyFile(originalFilePath, writePath)
+            } else if (el.isDirectory()) {
+                fs.promises.mkdir(path.join(urlDestination, file))
+                copyDirectory(path.join(urlSource, file), path.join(urlDestination, file))
+            }
+        });
+    });
+});
